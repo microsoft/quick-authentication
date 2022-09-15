89870b6954c529932f0b89261c6241202abfe920
Minggang Wang <minggangwang@microsoft.com>
2022-09-16 00:10:25 +0800

    [iOS] Init Microsoft Quick Auth Demo
    This patch initializes the demo for the Microsoft Quick Auth SDK on iOS
    platform, including:
    
    1. Microsoft Quick Auth SDK framework (arm64), which locates in
       MSQASignIn.framework/.
    2. The code of a demo representing the functionalities provided by the SDK.
    
    Please reference the README.md to set up the demo on iOS devices, and
    the instructions about integrating the binary framework into other
    third-party applications.

---
+++ demos/iOS/SampleAppiOS/SampleLoginViewController.m
@@ -0,0 +1,282 @@
+//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+#import <MSQASignIn/MSQAConfiguration.h>
+#import <MSQASignIn/MSQASignIn.h>
+#import <MSQASignIn/MSQASignInButton.h>
+
+#import "SampleAppDelegate.h"
+#import "SampleLoginViewController.h"
+#import "SampleMainViewController.h"
+
+@interface SampleLoginViewController () <
+    UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate,
+    UIPickerViewDataSource>
+
+@property(nonatomic, weak) IBOutlet MSQASignInButton *signInButton;
+
+@property(nonatomic, strong) UITableView *configurationTableView;
+
+@property(nonatomic, strong) NSArray *configurationTableCellLabel;
+
+@property(nonatomic, strong) NSArray<NSArray *> *configrationPickerLabel;
+
+@property(nonatomic, strong)
+    NSMutableArray<NSNumber *> *currentConfigurationState;
+
+@property(nonatomic, assign) NSUInteger selectedConfigurationIndex;
+
+@end
+
+@implementation SampleLoginViewController {
+  MSQASignIn *_msSignIn;
+}
+
++ (instancetype)sharedViewController {
+  static SampleLoginViewController *s_controller = nil;
+  static dispatch_once_t once;
+
+  dispatch_once(&once, ^{
+    s_controller =
+        [[SampleLoginViewController alloc] initWithNibName:@"SampleLoginView"
+                                                    bundle:nil];
+  });
+
+  return s_controller;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil
+                         bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    [MSQASignInButton class];
+    _configurationTableView = [[UITableView alloc] init];
+    _configurationTableView.delegate = self;
+    _configurationTableView.dataSource = self;
+
+    _configurationTableCellLabel = @[
+      @"Type",
+      @"Theme",
+      @"Size",
+      @"Text",
+      @"Shape",
+      @"Logo",
+    ];
+    _currentConfigurationState = [@[ @0, @0, @0, @0, @0, @0 ] mutableCopy];
+    _configrationPickerLabel = @[
+      @[ @"Standard", @"Icon" ], @[ @"Light", @"Dark" ],
+      @[ @"Large", @"Medium", @"Small" ],
+      @[
+        @"Sign in with Microsoft", @"Sign up with Microsoft",
+        @"Continue with Microsoft", @"Sign in"
+      ],
+      @[ @"Rectangular", @"Rounded", @"Pill" ],
+      @[ @"Both left", @"Brand left text center", @"Both center" ]
+    ];
+  }
+  return self;
+}
+
+- (void)setMSQASignIn:(MSQASignIn *)msSignIn {
+  _msSignIn = msSignIn;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Switch to main view if already logged in.
+  [_msSignIn getCurrentAccountWithCompletionBlock:^(
+                 MSQAAccountData *_Nullable account, NSError *_Nullable error) {
+    if (account && !error) {
+      SampleMainViewController *controller =
+          [SampleMainViewController sharedViewController];
+      [controller setAccountInfo:account msSignIn:_msSignIn];
+      [SampleAppDelegate setCurrentViewController:controller];
+    }
+  }];
+
+  // Padding View
+  UIView *paddingView = [[UIView alloc] init];
+  paddingView.translatesAutoresizingMaskIntoConstraints = NO;
+  [self.view addSubview:paddingView];
+  [NSLayoutConstraint activateConstraints:@[
+    [paddingView.widthAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.widthAnchor],
+    [paddingView.heightAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.heightAnchor
+                     multiplier:0.25],
+    [paddingView.centerXAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor],
+    [paddingView.bottomAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerYAnchor]
+  ]];
+
+  // Setup Button
+  self.signInButton.translatesAutoresizingMaskIntoConstraints = NO;
+  [NSLayoutConstraint activateConstraints:@[
+    [self.signInButton.widthAnchor constraintEqualToConstant:300],
+    [self.signInButton.heightAnchor constraintEqualToConstant:300],
+    [self.signInButton.bottomAnchor
+        constraintEqualToAnchor:paddingView.topAnchor],
+    [self.signInButton.centerXAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor]
+  ]];
+
+  // Setup Table View Configuration
+  [self.view addSubview:self.configurationTableView];
+  self.configurationTableView.translatesAutoresizingMaskIntoConstraints = NO;
+  self.configurationTableView.backgroundColor = [UIColor clearColor];
+  [NSLayoutConstraint activateConstraints:@[
+    [self.configurationTableView.widthAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.widthAnchor],
+    [self.configurationTableView.heightAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.heightAnchor
+                     multiplier:0.5],
+    [self.configurationTableView.topAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerYAnchor],
+    [self.configurationTableView.centerXAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.centerXAnchor]
+  ]];
+}
+
+- (IBAction)signIn:(id)sender {
+  [_msSignIn signInWithViewController:self
+                      completionBlock:^(MSQAAccountData *_Nonnull account,
+                                        NSError *_Nonnull error) {
+                        SampleMainViewController *controller =
+                            [SampleMainViewController sharedViewController];
+                        [controller setAccountInfo:account msSignIn:_msSignIn];
+                        [SampleAppDelegate setCurrentViewController:controller];
+                      }];
+}
+
+- (IBAction)getCurrentButton:(id)sender {
+  [_msSignIn getCurrentAccountWithCompletionBlock:^(
+                 MSQAAccountData *_Nullable account, NSError *_Nullable error) {
+    SampleMainViewController *controller =
+        [SampleMainViewController sharedViewController];
+    [controller setAccountInfo:account msSignIn:_msSignIn];
+    [SampleAppDelegate setCurrentViewController:controller];
+  }];
+}
+
+- (void)applyUserPreferences {
+  self.signInButton.type = self.currentConfigurationState[0].intValue;
+  self.signInButton.theme = self.currentConfigurationState[1].intValue;
+  self.signInButton.size = self.currentConfigurationState[2].intValue;
+  self.signInButton.text = self.currentConfigurationState[3].intValue;
+  self.signInButton.shape = self.currentConfigurationState[4].intValue;
+  self.signInButton.logo = self.currentConfigurationState[5].intValue;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  return self.configurationTableCellLabel.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSString *label = self.configurationTableCellLabel[indexPath.row];
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:label];
+  if (!cell) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                  reuseIdentifier:label];
+  }
+  cell.textLabel.text = label;
+  cell.detailTextLabel.text =
+      self.configrationPickerLabel[indexPath.row]
+                                  [self.currentConfigurationState[indexPath.row]
+                                       .intValue];
+  return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
+  self.selectedConfigurationIndex = indexPath.row;
+  UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+
+  UIPickerView *pickerView = [[UIPickerView alloc] init];
+  pickerView.dataSource = self;
+  pickerView.delegate = self;
+
+  UIViewController *viewController = [[UIViewController alloc] init];
+  viewController.view = pickerView;
+
+  NSString *title =
+      [NSString stringWithFormat:@"Select %@", cell.textLabel.text];
+
+  UIAlertController *alertController = [UIAlertController
+      alertControllerWithTitle:title
+                       message:nil
+                preferredStyle:UIAlertControllerStyleActionSheet];
+  [alertController setValue:viewController forKey:@"contentViewController"];
+  [alertController
+      addAction:[UIAlertAction actionWithTitle:@"Done"
+                                         style:UIAlertActionStyleDefault
+                                       handler:nil]];
+  [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - UIPickerViewDataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+  return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView
+    numberOfRowsInComponent:(NSInteger)component {
+  return self.configrationPickerLabel[self.selectedConfigurationIndex].count;
+}
+
+#pragma mark - UIPickerViewDelegate
+
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row
+            forComponent:(NSInteger)component {
+  return self.configrationPickerLabel[self.selectedConfigurationIndex][row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView
+      didSelectRow:(NSInteger)row
+       inComponent:(NSInteger)component {
+  UITableViewCell *cell = [self.configurationTableView
+      cellForRowAtIndexPath:[NSIndexPath
+                                indexPathForRow:self.selectedConfigurationIndex
+                                      inSection:0]];
+  cell.detailTextLabel.text =
+      self.configrationPickerLabel[self.selectedConfigurationIndex][row];
+  self.currentConfigurationState[self.selectedConfigurationIndex] =
+      [NSNumber numberWithLong:row];
+  [self applyUserPreferences];
+}
+
+@end
---
+++ demos/iOS/SampleAppiOS/SampleMainViewController.m
@@ -0,0 +1,160 @@
+//------------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.
+// All rights reserved.
+//
+// This code is licensed under the MIT License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+#import "SampleMainViewController.h"
+
+#import <MSQASignIn/MSQAAccountData.h>
+#import <MSQASignIn/MSQASignIn.h>
+#import <MSQASignIn/MSQASilentTokenParameters.h>
+
+#import "SampleAppDelegate.h"
+#import "SampleLoginViewController.h"
+
+@implementation SampleMainViewController {
+  MSQAAccountData *_accountData;
+  MSQASignIn *_msSignIn;
+}
+
++ (instancetype)sharedViewController {
+  static SampleMainViewController *s_controller = nil;
+  static dispatch_once_t once;
+
+  dispatch_once(&once, ^{
+    s_controller =
+        [[SampleMainViewController alloc] initWithNibName:@"SampleMainView"
+                                                   bundle:nil];
+  });
+
+  return s_controller;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil
+               bundle:(NSBundle *)nibBundleOrNil {
+  if (!(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+    return nil;
+  }
+  return self;
+}
+
+- (void)setUserPhoto:(UIImage *)photo {
+  _profileImageView.image = photo;
+  _profileImageView.layer.cornerRadius = _profileImageView.frame.size.width / 2;
+  _profileImageView.layer.borderWidth = 4.0f;
+  _profileImageView.layer.borderColor = UIColor.whiteColor.CGColor;
+  _profileImageView.clipsToBounds = YES;
+}
+
+- (void)setAccountInfo:(MSQAAccountData *)accountData
+              msSignIn:(MSQASignIn *)msSignIn {
+  _accountData = accountData;
+  _msSignIn = msSignIn;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  _nameLabel.text =
+      [NSString stringWithFormat:@"Welcome, %@", _accountData.userName];
+  _fullNameLabel.text =
+      [NSString stringWithFormat:@"Full name: %@", _accountData.fullName];
+  _idLabel.text = [NSString stringWithFormat:@"Id: %@", _accountData.userId];
+
+  if (_accountData.photo) {
+    NSData *data = [[NSData alloc]
+        initWithBase64EncodedString:_accountData.photo
+                            options:
+                                NSDataBase64DecodingIgnoreUnknownCharacters];
+    [self setUserPhoto:[UIImage imageWithData:data]];
+    return;
+  }
+  [self setUserPhoto:[UIImage imageNamed:@"no_photo"]];
+}
+
+- (IBAction)signOut:(id)sender {
+  [_msSignIn signOutWithCompletionBlock:^(NSError *_Nullable error) {
+    if (error)
+      NSLog(@"Error:%@", error.description);
+  }];
+  [SampleAppDelegate setCurrentViewController:[SampleLoginViewController
+                                                  sharedViewController]];
+}
+
+- (IBAction)fetchTokenSilent:(id)sender {
+  MSQASilentTokenParameters *parameters =
+      [[MSQASilentTokenParameters alloc] initWithScopes:@[ @"User.Read" ]];
+  [_msSignIn acquireTokenSilentWithParameters:parameters
+                              completionBlock:^(MSQAAccountData *account,
+                                                NSError *error) {
+                                self->_tokenLabel.text =
+                                    [NSString stringWithFormat:@"ID token: %@",
+                                                               account.idToken];
+                              }];
+}
+
+- (IBAction)fetchTokenInteractive:(id)sender {
+  MSQAWebviewParameters *webParameters = [[MSQAWebviewParameters alloc]
+      initWithAuthPresentationViewController:self];
+  MSQAInteractiveTokenParameters *parameters =
+      [[MSALInteractiveTokenParameters alloc]
+             initWithScopes:@[ @"User.Read", @"Calendars.Read" ]
+          webviewParameters:webParameters];
+
+  [_msSignIn acquireTokenWithParameters:parameters
+                        completionBlock:^(MSQAAccountData *_Nullable account,
+                                          NSError *_Nullable error) {
+                          self->_tokenLabel.text =
+                              [NSString stringWithFormat:@"ID token: %@",
+                                                         account.idToken];
+                        }];
+}
+
+- (IBAction)getCurrentAccount:(id)sender {
+  [_msSignIn getCurrentAccountWithCompletionBlock:^(
+                 MSQAAccountData *_Nullable account, NSError *_Nullable error) {
+    if (account) {
+      NSString *message =
+          [NSString stringWithFormat:@"FullName: %@\nEmail: %@",
+                                     account.fullName, account.userName];
+      [self showAlertWithMessage:message];
+      return;
+    }
+    [self showAlertWithMessage:@"None available account"];
+  }];
+}
+
+- (void)showAlertWithMessage:(NSString *)message {
+  UIAlertController *controller = [UIAlertController
+      alertControllerWithTitle:@"Current account"
+                       message:message
+                preferredStyle:UIAlertControllerStyleActionSheet];
+  UIAlertAction *action =
+      [UIAlertAction actionWithTitle:@"OK"
+                               style:UIAlertActionStyleDefault
+                             handler:nil];
+  [controller addAction:action];
+  [self presentViewController:controller animated:YES completion:nil];
+}
+
+@end
